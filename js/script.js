0f4c8eec33b725434c765c58cd343f10649d8f41
lucas souza <lucas2469y@gmail.com>
2025-05-25 11:42:38 -0300

    fazendo uns ajustes no css e adicionando o media query

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 function validarEmail() {
-    const emailInput = document.getElementById("email");
-    let  email = document.getElementById("email").value;
-     let erro = document.getElementById("erro");
-    let  regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // expressão regular para validar o formato básico de um email (sem espaços, com um @ e um ponto)
+    let emailInput = document.getElementById("email");
+    let email = document.getElementById("email").value;
+    let erro = document.getElementById("erro");
+    let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // expressão regular para validar o formato básico de um email (sem espaços, com um @ e um ponto)
 
     if (!regexEmail.test(email)) { // verifica se o e-mail nao corresponde ao padrão da regex (ou seja, se é inválido)
-      erro.style.visibility = "visible";
-      emailInput.classList.add("erro");
+        erro.style.visibility = "visible";
+        emailInput.classList.add("erro");
 
     } else {
-      erro.style.visibility = "hidden";
-      emailInput.classList.remove("erro");
-      // Aqui você pode adicionar o que quiser fazer com o e-mail válido
-      alert("Email válido! Você será notificado.");
+        erro.style.visibility = "hidden";
+        emailInput.classList.remove("erro");
+        alert("Email válido! Você será notificado.");  // aqui você pode dicionar que quiser fazer com o e-mail válido
     }
-  }
+}
